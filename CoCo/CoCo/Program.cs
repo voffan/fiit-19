532eeca44d70d45f070a8af0fb583d9b57280abf
milkis961 <ivanova544252@mail.ru>
2022-02-03 10:53:43 +0900

    created project

--- CoCo/CoCo/Program.cs
+++ CoCo/CoCo/Program.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,15 +6,14 @@ using System.Windows.Forms;
 
 namespace CoCo
 {
-    static class Program
+    internal static class Program
     {
         /// <summary>
-        ///  The main entry point for the application.
+        /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
