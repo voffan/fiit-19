c9cc6792777a5b03852fd5b8f7410bccefa66fcc
Sobyanin Seryoga <ioruichi98@gmail.com>
2022-02-03 17:05:02 +0900

    update of classes #1

--- SportAchievements/SportAchievements/Classes/Competition.cs
+++ SportAchievements/SportAchievements/Classes/Competition.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportAchievements.Classes
 {
     public class Competition
     {
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Name { get; set; }
         public DateTime DateBeginning { get; set; } 
         public DateTime DateEnding { get; set; } 
--- SportAchievements/SportAchievements/Classes/Result.cs
+++ SportAchievements/SportAchievements/Classes/Result.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportAchievements.Classes
 {
     public class Result
     {
         public int Id { get; set; }
+        [Range(1, 3)]
         public int Place { get; set; }
         public int points { get; set; }
         public KindOfSport sport { get; set; }
