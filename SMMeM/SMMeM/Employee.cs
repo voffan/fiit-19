9bcfa4fb0663921f4c5d628833cea838babfab4a
Aital <90888185+latiAM@users.noreply.github.com>
2022-02-17 10:20:01 +0900

    Add files via upload

---
+++ SMMeM/SMMeM/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMM
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [MaxLength(255)]
+        public string Login { get; set; }
+        [MaxLength(255)]
+        public string Password { get; set; }
+        [MaxLength(11)]
+        public DateTime Birthday { get; set; }
+        public Position Position { get; set; }
+    }
+}
---
+++ SMMeM/SMMeM/Position.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMM
+{
+    public enum Position
+    {
+        Director,
+        Employee
+    }
+}
