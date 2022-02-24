8310558950ca655d40554f0f8efbd9e9094e0bcb
milkis961 <ivanova544252@mail.ru>
2022-02-24 10:11:11 +0900

    mb and hdd list forms

---
+++ CoCo/CoCo/Form_Hdds.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoCo
+{
+    public partial class Form_Hdds : Form
+    {
+        public Form_Hdds()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Hdds_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = new Context().Hdds.ToList();
+            dataGridView1.Columns[0].HeaderText = "Номер";
+            dataGridView1.Columns[1].HeaderText = "Название";
+            dataGridView1.Columns[2].HeaderText = "Объем";
+            dataGridView1.Columns[3].HeaderText = "Производитель";
+        }
+    }
+}
---
+++ CoCo/CoCo/Form_Motherboards.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoCo
+{
+    public partial class Form_Motherboards : Form
+    {
+        public static Context context = new Context();
+        public Form_Motherboards()
+        {
+            InitializeComponent();
+        }
+
+        private void Motherboards_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = context.Motherboards.ToList();
+            //            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[0].HeaderText = "Номер";
+            dataGridView1.Columns[1].HeaderText = "Название";
+            dataGridView1.Columns[2].HeaderText = "Производитель";
+            /*            for (int i = 0; i < dataGridView1.ColumnCount; i++)
+                        {
+                            for (int _i = 0; _i < dataGridView1.RowCount; _i++)
+                            {
+                                dataGridView1.Rows.Add();
+                            }
+                        }*/
+        }
+    }
+}
