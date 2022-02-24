76c9ce57de8dc45a6214b3abf2d328bab8a7e43c
SivtsevN <91646499+SivtsevN@users.noreply.github.com>
2022-02-24 13:13:35 +0900

    Add files via upload

---
+++ SMMeMnew/SMMeM/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMM
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Login { get; set; }
+        [MaxLength(50)]
+        public string Password { get; set; }
+        public DateTime Birthday { get; set; }
+        public Position Position { get; set; }
+        public int EmployerId { get; set; }
+        public virtual Employer Employer { get; set; }
+        public virtual List<Error> Errors { get; set; }
+        public virtual List<Solution> Solutions { get; set; }
+
+    }
+}
