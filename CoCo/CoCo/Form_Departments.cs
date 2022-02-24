260cf5b6ed765406fc5d1a591a53e0d0ae9ea582
Hompsandaara <90888301+Hompsandaara@users.noreply.github.com>
2022-02-24 10:27:46 +0900

    Sandaara

---
+++ CoCo/CoCo/Form_Departments.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoCo
+{
+    public partial class Form_Departments : Form
+    {
+        public Form_Departments()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Departments_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = new Context().Departments.ToList();
+            dataGridView1.Columns[0].HeaderText = "Номер";
+            dataGridView1.Columns[1].HeaderText = "Название";
+        }
+    }
+}
---
+++ CoCo/CoCo/Form_Employees.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoCo
+{
+    public partial class Form_Employees : Form
+    {
+        public Form_Employees()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_Departments_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = new Context().Departments.ToList();
+            dataGridView1.Columns[0].HeaderText = "Номер сотрудника";
+            dataGridView1.Columns[1].HeaderText = "ФИО";
+            dataGridView1.Columns[2].HeaderText = "Номер отдела";
+            dataGridView1.Columns[3].HeaderText = "Название отдела";
+
+        }
+    }
+}
