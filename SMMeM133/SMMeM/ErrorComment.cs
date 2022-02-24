5b35799933327ab038f03c21e05b14e63d2ce3d0
SivtsevN <91646499+SivtsevN@users.noreply.github.com>
2022-02-24 13:25:29 +0900

    Add files via upload

---
+++ SMMeM133/SMMeM/ErrorComment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace SMM
+{
+    public class ErrorComment
+    {
+        public int Id { get; set; }
+        [MaxLength(500)]
+        public string Text { get; set; }
+        public int ErrorId { get; set; }
+        public virtual Error Error { get; set; }
+    }
+}
