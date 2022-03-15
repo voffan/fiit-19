e1642bf1ba86f880215f983396f050f83f9ce60d
milkis961 <87695024+milkis961@users.noreply.github.com>
2022-03-15 23:03:01 +0900

    somestuff

--- CoCo/CoCo/Program.cs
+++ CoCo/CoCo/Program.cs
@@ -16,7 +16,7 @@ namespace CoCo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Auth());
+            Application.Run(new Form1());
         }
     }
 }
