8d4e3591981bce677ba26554d2446676d9057011
Hompsandaara <90888301+Hompsandaara@users.noreply.github.com>
2022-03-24 12:33:11 +0900

    AddEmplоy

--- CoCo/CoCo/Forms/Form_Employees.cs
+++ CoCo/CoCo/Forms/Form_Employees.cs
@@ -20,17 +20,33 @@ namespace CoCo
 
         private void Form_Employees_Load(object sender, EventArgs e)
         {
+            initTable();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AddEmployees adh = new AddEmployees();
+            adh.MdiParent = this.MdiParent;
+            adh.FormClosing += MdiChildClose;
+            adh.Show();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void initTable()
+        {
             dataGridView1.DataSource = new Context().Employees.ToList();
             dataGridView1.Columns[0].HeaderText = "Номер сотрудника";
             dataGridView1.Columns[1].HeaderText = "ФИО";
             dataGridView1.Columns[2].HeaderText = "Номер отдела";
             dataGridView1.Columns[3].HeaderText = "Название отдела";
-
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void MdiChildClose(Object sender, FormClosingEventArgs e)
         {
-            new AddEmployees().ShowDialog();
+            initTable();
         }
     }
 }
