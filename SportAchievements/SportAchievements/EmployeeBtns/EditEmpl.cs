984eba1d332bd08f0cb0a39cb7e02652a5791225
abbadon321 <ioruichi98@gmail.com>
2022-04-14 11:19:57 +0900

    upgrade EditSprtmn, EditEmpl

--- SportAchievements/SportAchievements/EmployeeBtns/EditEmpl.cs
+++ SportAchievements/SportAchievements/EmployeeBtns/EditEmpl.cs
@@ -8,17 +8,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SportAchievements.Components;
+using SportAchievements.Classes;
 
 namespace SportAchievements.EmployeeBtns
 {
     public partial class EditEmpl : Form
     {
         int Id;
+        Employee empl;
 
         public EditEmpl(int Id)
         {
             this.Id = Id;
             InitializeComponent();
+            empl = (new Context()).Employees.Find(Id);
         }
 
         private void Save_Click(object sender, EventArgs e)
@@ -41,10 +44,20 @@ namespace SportAchievements.EmployeeBtns
 
         private void EditEmpl_Load(object sender, EventArgs e)
         {
+            if (empl == null)
+            {
+                MessageBox.Show("Сотрудник не найден!");
+                Close();
+            }
+            lgn.Text = empl.Login;
+            psw.Text = empl.Pwd;
+            FIO.Text = empl.Name;
+            bdate.Value = empl.BirthDate;
             pos.DataSource = new BindingSource(Classes.DescriptionAttributes<Classes.Position>.RetrieveAttributes(), null);
             pos.DisplayMember = "Key";
             pos.ValueMember = "Value";
-
+            var obj = (from KeyValuePair<string, string> s in pos.Items where s.Value == Enum.GetName(typeof(SportAchievements.Classes.Position), empl.Position) select s).FirstOrDefault();
+            pos.SelectedItem = obj;
         }
 
         private void Cancel_Click(object sender, EventArgs e)
--- SportAchievements/SportAchievements/SportsmanBtns/EditSportsman.cs
+++ SportAchievements/SportAchievements/SportsmanBtns/EditSportsman.cs
@@ -8,28 +8,41 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using SportAchievements.Components;
+using SportAchievements.Classes;
 
 namespace SportAchievements.SportsmanBtns
 {
     public partial class EditSportsman : Form
     {
         int Id;
+        Sportsman sp;
+        
 
         public EditSportsman(int Id)
         {
             this.Id = Id;
             InitializeComponent();
-
+            sp = (new Context()).Sportsmen.Find(Id);
         }
 
         private void EditSportsman_Load(object sender, EventArgs e)
         {
+            if(sp == null)
+            {
+                MessageBox.Show("Спортсмен не найден!");
+                Close();
+            }
+            FIO.Text = sp.Name;
+            bdate.Value = sp.BirthDate;
             gen.DataSource = new BindingSource(Classes.DescriptionAttributes<Classes.Gender>.RetrieveAttributes(), null);
             gen.DisplayMember = "Key";
             gen.ValueMember = "Value";
+            var obj = (from KeyValuePair<string, string> s in gen.Items where s.Value == Enum.GetName(typeof(SportAchievements.Classes.Gender), sp.Gender) select s).FirstOrDefault();
+            gen.SelectedItem = obj;
             comboBox1.DataSource = (new Context()).WeightCategories.ToList();
             comboBox1.DisplayMember = "Name";
             comboBox1.ValueMember = "Id";
+            comboBox1.SelectedItem = (from WeightCategory wc in comboBox1.Items where wc.Id == sp.WeightCategoryId select wc).FirstOrDefault();
         }
 
         private void Save_Click(object sender, EventArgs e)
