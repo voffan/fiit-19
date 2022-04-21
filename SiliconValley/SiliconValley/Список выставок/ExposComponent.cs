09eb960eef6f233086a6751fc5490fb438a89506
NEFUstudents <study.svfu@gmail.com>
2022-04-21 10:32:04 +0900

    sdcdcscsd

---
+++ SiliconValley/SiliconValley/Список выставок/ExposComponent.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiliconValley
+{
+    class ExposComponent
+    {
+        public static void Add<T>(T obj) where T : class
+        {
+            using (var db = new Context())
+            {
+                db.Set<T>().Add(obj);
+                db.SaveChanges();
+            }
+        }
+
+        public static void Edit<T>(T obj) where T:class
+        {
+            using (var db = new Context())
+            {
+                T a = obj;
+                if (a != null)
+                {
+                    db.Entry(a).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else throw new Exception("");
+            }
+        }
+
+        public static void Delete<T>(T obj) where T : class
+        {
+            using (var db = new Context())
+            {
+                db.Set<T>().Remove(obj);
+                db.SaveChanges();
+            }
+        }
+
+        public static T GetObjById<T>(int index) where T : class
+        {
+            Context c = new Context();
+            T a = (from obj in c.Set<T>()
+                        where ((IId)obj).Id == index
+                        select obj).FirstOrDefault();
+            return a;
+        }
+
+    }
+}
