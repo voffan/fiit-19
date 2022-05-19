02576f2099367d0f29cfeb074a6bdd0436c679fe
egor dyakonov <egordyakonov2000@gmail.com>
2022-05-19 11:04:23 +0900

    change peripheral

--- CoCo/CoCo/Classes/PeripheralLogic.cs
+++ CoCo/CoCo/Classes/PeripheralLogic.cs
@@ -28,5 +28,24 @@ namespace CoCo.Classes
             context.Peripherals.Remove(cpu);
             context.SaveChanges();
         }
+
+        public static void PeripheralChange(string name, string manu, int empl, int value)
+        {
+            using (var context = new Context())
+            {
+                //Peripheral per = (from Peripheral per1 in context.Peripherals where per1.Id == perId select per1).FirstOrDefault();    
+                //Peripheral per = (Peripheral)context.Device.FirstOrDefault(x => x.Id == perId);
+                var per = context.Peripherals.Find(value);
+
+                //Peripheral per = context.Peripherals.FirstOrDefault(x => x.Id == perId);
+                per.Name = name;
+                per.Manufacturer = manu;
+                per.Employee = context.Employees.Find(empl);
+
+
+                context.SaveChanges();
+            }
+
+        }
     }
 }
