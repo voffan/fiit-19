0e9bcb3a6fcf8ba7489dfbd02e72aea7de733af2
egor dyakonov <egordyakonov2000@gmail.com>
2022-05-19 11:31:54 +0900

    Change Cpus

---
+++ CoCo/CoCo/Forms/ChangeCpu.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CoCo.Classes;
+
+namespace CoCo.Forms
+{
+    public partial class ChangeCpu : Form
+    {
+        object cpuId;
+        public ChangeCpu(object _cpu)
+        {
+            InitializeComponent();
+            cpuId = _cpu;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string name = textBox1.Text;
+            string manu= textBox3.Text;
+            decimal freq = Convert.ToDecimal(textBox2.Text);
+            CpuLogic.CpuChange(name, freq, manu, cpuId);
+            Close();
+        }
+    }
+}
--- CoCo/CoCo/Forms/Form_Cpus.Designer.cs
+++ CoCo/CoCo/Forms/Form_Cpus.Designer.cs
@@ -33,6 +33,7 @@ namespace CoCo
             this.button_add = new System.Windows.Forms.Button();
             this.button_delete = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -72,11 +73,22 @@ namespace CoCo
             this.textBox1.TabIndex = 4;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 319);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Изменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
             // Form_Cpus
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(675, 351);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button_delete);
             this.Controls.Add(this.button_add);
@@ -96,5 +108,6 @@ namespace CoCo
         private System.Windows.Forms.Button button_add;
         private System.Windows.Forms.Button button_delete;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
